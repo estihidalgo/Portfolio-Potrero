4207dd3786cfc9cb3185122b188ed99bcd2f1aab
estihidalgo <hidalgo.esteban98@gmail.com>
2022-08-01 20:28:12 -0300

    portfolio final

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+
+function seleccionar(link){
+    var opciones = document.querySelectorAll("#links a");
+    opciones[0].className = ""
+    opciones[1].className = ""
+    opciones[3].className = ""
+    link.className = "seleccionado"
+
+var x = document.getElementById("#nav")
+x.className = "";
+
+}
+
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if (x.className === ""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
+
